31812a2519b082ad908df2da06ccd6987e07a5c7
Robbie Gates <rgates@atlassian.com>
2022-01-03 17:29:08 +1100

    Start experimenting with mousekeys
    I haven't got the acceleration how i like it yet, but nor do i really
    understand what the parameters are doing.

---
+++ keyboards/massdrop/alt/keymaps/robbieg/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// https://docs.qmk.fm/#/feature_mouse_keys?id=kinetic-mode
+#define MK_KINETIC_SPEED
+// #define MOUSEKEY_DELAY 50
+#define MOUSEKEY_INTERVAL 50
+// #define MOUSEKEY_MOVE_DELTA 5
+#define MOUSEKEY_INITIAL_SPEED 10
